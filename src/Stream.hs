5b7e20e4bcf53fbb8d8154ba233850da362fea0e
Michael Pinkham <mpinkham3@gmail.com>
2017-11-27 01:17:24 -0500

    Add packHexBytes

--- src/Stream.hs
+++ src/Stream.hs
@@ -4,6 +4,7 @@ module Stream
 
 import qualified Data.ByteString.Lazy.Char8 as C8
 import qualified Data.Char as C
+import qualified Data.Bits as B
 
 hexToRaw :: C8.ByteString -> C8.ByteString
 hexToRaw = packHexBytes . (C8.map hexCharToByte)
@@ -12,4 +13,12 @@ hexCharToByte :: C.Char -> C.Char
 hexCharToByte = C.chr . C.digitToInt
 
 packHexBytes :: C8.ByteString -> C8.ByteString
-packHexBytes = id
+packHexBytes x 
+   | x == C8.empty = x
+   | otherwise     = C8.cons c $ packHexBytes rest 
+   where
+    two = C8.take 2 x
+    rest = C8.drop 2 x
+    a = B.shiftL (C.ord . C8.head $ two) 4
+    b = (C.ord . C8.last $ two) B..&. 0x0F
+    c = C.chr $ a B..&. b
